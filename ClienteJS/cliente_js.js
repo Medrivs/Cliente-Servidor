306b47b0953090c42b0760fa4151e134eba83068
Alan Medina Rivas <alnmdnrvs@gmail.com>
2026-09-22 12:40:02 -0600

    Update cliente_js.js

--- ClienteJS/cliente_js.js
+++ ClienteJS/cliente_js.js
@@ -1,10 +1,9 @@
 const net = require('net');
 const readline = require('readline');
 
-// Configuración de la conexión
 const PORT = 81; 
 const HOST = '192.168.218.150'; 
-const MI_NOMBRE = 'Rivassss'; // <-- ¡PON TU NOMBRE AQUÍ!
+const MI_NOMBRE = 'Alan Medina Rivas'; 
 
 const cliente = new net.Socket();
 
@@ -18,25 +17,22 @@ const rl = readline.createInterface({
 cliente.connect(PORT, HOST, () => {
     console.log(`¡Conectado exitosamente al servidor Java en ${HOST}:${PORT}!`);
     console.log("Escribe tu mensaje y presiona Enter (Escribe 'salir' para terminar).\n");
-    rl.prompt(); // Muestra el cursor para escribir
+    rl.prompt(); 
 });
 
 // 2. Escuchar los mensajes que manda el Servidor (Java)
 cliente.on('data', (data) => {
-    // Solo imprimimos los datos tal cual llegan, sin ponerle "Servidor Java dice:"
     console.log(`\n${data.toString().trim()}`);
     rl.prompt();
 });
 // 3. Leer lo que tú escribes y enviarlo
 rl.on('line', (linea) => {
     if (linea.toLowerCase() === 'salir') {
-        cliente.write(`${MI_NOMBRE} se ha desconectado.\n`); // Avisamos a los demás
+        cliente.write(`${MI_NOMBRE} se ha desconectado.\n`); 
         console.log("Cerrando la conexión...");
         cliente.destroy();
         process.exit(0);
     } else {
-        // ¡LA NUEVA MAGIA AQUÍ! 
-        // Juntamos tu nombre, dos puntos, tu mensaje y el salto de línea
         cliente.write(`${MI_NOMBRE}:${linea}\n`);
         rl.prompt();
     }
